ec6aeec8620d54999eee2455bbbf6d6b1a4bb5c4
Douglas Tertuliano <douglasjtds@gmail.com>
2024-08-12 14:17:55 -0300

    feat: add SidebarSheet component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
+import SideBarSheet from "./sidebar-sheet"
+import { Sheet, SheetTrigger } from "./ui/sheet"
 import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 
@@ -8,9 +10,15 @@ const Header = () => {
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
-        <Button size="icon" variant="outline">
-          <MenuIcon></MenuIcon>
-        </Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SideBarSheet />
+        </Sheet>
       </CardContent>
     </Card>
   )
---
+++ app/_components/sidebar-sheet.tsx
@@ -0,0 +1,71 @@
+import { Button } from "./ui/button"
+import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
+import { quickSearchOptions } from "../_constants/search"
+import { Avatar } from "./ui/avatar"
+import { AvatarImage } from "@radix-ui/react-avatar"
+import Link from "next/link"
+import Image from "next/image"
+
+const SideBarSheet = () => {
+  return (
+    <SheetContent className="overflow-y-auto">
+      <SheetHeader>
+        <SheetTitle className="text-left">Menu</SheetTitle>
+      </SheetHeader>
+
+      <div className="flex items-center gap-3 border-b border-solid py-5">
+        <Avatar>
+          <AvatarImage src="https://plus.unsplash.com/premium_photo-1664536392779-049ba8fde933?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </Avatar>
+
+        <div>
+          <p className="font-bold">Douglas Tertuliano</p>
+          <p className="text-xs">dojotes_developer@proton.me</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Início
+            </Link>
+          </Button>
+        </SheetClose>
+        <Button className="justify-start gap-2" variant="ghost">
+          <CalendarIcon size={18} />
+          Agendamentos
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-4 border-b border-solid py-5">
+        {quickSearchOptions.map((option) => (
+          <Button
+            key={option.title}
+            className="justify-start gap-2"
+            variant="ghost"
+          >
+            <Image
+              width={18}
+              height={18}
+              src={option.imageUrl}
+              alt={option.title}
+            />
+            {option.title}
+          </Button>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-4 border-b border-solid py-5">
+        <Button variant="ghost" className="justify-start gap-2">
+          <LogOutIcon size={18} />
+          Sair da conta
+        </Button>
+      </div>
+    </SheetContent>
+  )
+}
+
+export default SideBarSheet
