13af33ca732b6747f4ddab94c435bee09f75c172
Douglas Tertuliano <douglasjtds@gmail.com>
2024-08-13 18:10:22 -0300

    feat: clickable logo redirects to homepage now

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,12 +4,15 @@ import SideBarSheet from "./sidebar-sheet"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        <Link href="/">
+          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        </Link>
 
         <Sheet>
           <SheetTrigger asChild>
